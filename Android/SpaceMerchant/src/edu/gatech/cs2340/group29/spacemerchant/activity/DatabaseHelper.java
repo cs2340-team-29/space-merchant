2de09caacc736d3c2606bd6e6c9da8fe6e9e72f9
rmowlavi <rmowlavi@gmail.com>
2012-10-07 23:46:20 -0400

    Added wrapper function for SQLite database

---
+++ Android/SpaceMerchant/src/edu/gatech/cs2340/group29/spacemerchant/activity/DatabaseHelper.java
@@ -0,0 +1,41 @@
+package edu.gatech.cs2340.group29.spacemerchant.activity;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper
+{
+
+		public DatabaseHelper(Context context)
+		{			
+			super(context, "spaceMerchant" , null, 1);
+			
+		}
+
+	
+		public void onCreate(SQLiteDatabase db) 
+		{
+			final String query = ""                     
+				+ "create table tb_game( "
+			    + "    game integer primary key autoincrement "
+				+ "    playerName string not null" 
+			    + "    pilotSkillPoints integer not null "
+			    + "    fighterSkillPoints integer not null"
+			    + "    traderSkillPoints integer not null" 
+			    + "    engineerSkillPoints integer not null "
+			    + "    planet integer not null"
+				+ "    difficulty integer not null";
+			
+			db.execSQL(query);
+		}
+
+
+		public void onUpgrade(SQLiteDatabase arg0, int arg1, int arg2) 
+		{
+	
+			
+		}
+		
+	
+}
