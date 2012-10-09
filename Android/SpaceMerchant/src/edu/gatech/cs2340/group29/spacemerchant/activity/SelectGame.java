f19004e2cda4524a2356731f22dc064fd9be5465
IAmKevin <kshu7@gatech.edu>
2012-10-09 16:17:14 -0300

    Update Android/SpaceMerchant/src/edu/gatech/cs2340/group29/spacemerchant/activity/SelectGame.java

--- Android/SpaceMerchant/src/edu/gatech/cs2340/group29/spacemerchant/activity/SelectGame.java
+++ Android/SpaceMerchant/src/edu/gatech/cs2340/group29/spacemerchant/activity/SelectGame.java
@@ -1,10 +1,18 @@
 
 package edu.gatech.cs2340.group29.spacemerchant.activity;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
 import edu.gatech.cs2340.group29.spacemerchant.R;
+import edu.gatech.cs2340.group29.spacemerchant.adapter.SelectGameAdapter;
+import edu.gatech.cs2340.group29.spacemerchant.model.Player;
 
 public class SelectGame extends Activity
 {
@@ -14,6 +22,12 @@ public class SelectGame extends Activity
     {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_select_game );
+        
+        ArrayList<Player> p = new ArrayList<Player>();
+        p.add(new Player());
+        p.add(new Player());
+        SelectGameAdapter s = new SelectGameAdapter(this, R.layout.activity_select_game, p);
+        ((ListView) this.findViewById(R.id.gameList)).setAdapter(s);
     }
     
     @Override
@@ -21,5 +35,15 @@ public class SelectGame extends Activity
     {
         getMenuInflater().inflate( R.menu.activity_select_game, menu );
         return true;
+    }
+    
+    class SelectGameListener implements OnItemClickListener{
+
+    	public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+				long arg3) {
+			// TODO Auto-generated method stub
+			
+		}
+    	
     }
 }
