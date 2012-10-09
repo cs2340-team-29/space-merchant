e598a106f620009fc136658af210c1ac994dbe74
Conner Bryan <conner@pixuli.com>
2012-10-09 13:54:24 -0400

    'new'

---
+++ Android/SpaceMerchant/src/edu/gatech/cs2340/group29/spacemerchant/activity/ShipConfig.java
@@ -0,0 +1,7 @@
+package edu.gatech.cs2340.group29.spacemerchant.activity;
+
+import android.app.Activity;
+
+public class ShipConfig extends Activity {
+
+}
---
+++ Android/SpaceMerchant/src/edu/gatech/cs2340/group29/spacemerchant/util/PlayerDataSource.java
@@ -0,0 +1,35 @@
+
+package edu.gatech.cs2340.group29.spacemerchant.util;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+
+public class PlayerDataSource
+{
+    private SQLiteDatabase database;
+    private DatabaseHelper databaseHelper;
+    
+    public PlayerDataSource( Context context )
+    {
+        databaseHelper = new DatabaseHelper( context );
+    }
+    
+    public void open() throws SQLiteException
+    {
+        database = databaseHelper.getWritableDatabase();
+    }
+    
+    public void close()
+    {
+        databaseHelper.close();
+    }
+    
+    // public Player createPlayer(String name,
+    // {
+    
+    // }
+    
+    // public void
+    
+}
