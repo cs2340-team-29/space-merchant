334dc3cbe70cb177203a88a37cebb1f10a1d62df
Harrison Katz <hjkatz03@gmail.com>
2012-10-10 10:35:06 -0400

    Added ship config and layout

--- Android/SpaceMerchant/src/edu/gatech/cs2340/group29/spacemerchant/activity/ShipConfig.java
+++ Android/SpaceMerchant/src/edu/gatech/cs2340/group29/spacemerchant/activity/ShipConfig.java
@@ -1,7 +1,31 @@
+
 package edu.gatech.cs2340.group29.spacemerchant.activity;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import edu.gatech.cs2340.group29.spacemerchant.R;
 
-public class ShipConfig extends Activity {
-
+public class ShipConfig extends Activity
+{
+    
+    @Override
+    public void onCreate( Bundle savedInstanceState )
+    {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_ship_config );
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu( Menu menu )
+    {
+        getMenuInflater().inflate( R.menu.activity_ship_config, menu );
+        return true;
+    }
+    
+    public void doneButtonClicked( View v )
+    {
+        // do stuff here, send to main screen, save game, etc...
+    }
 }
