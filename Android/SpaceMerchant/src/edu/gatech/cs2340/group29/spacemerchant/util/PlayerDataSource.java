3c5ca7313634b6aca0a12707370faf48387e70f7
rmowlavi <rmowlavi@gmail.com>
2012-10-10 19:49:09 -0400

    Added getPlayerByID, cursorToPlayer methods to PlayerDataSource class

--- Android/SpaceMerchant/src/edu/gatech/cs2340/group29/spacemerchant/util/PlayerDataSource.java
+++ Android/SpaceMerchant/src/edu/gatech/cs2340/group29/spacemerchant/util/PlayerDataSource.java
@@ -1,58 +1,101 @@
-
 package edu.gatech.cs2340.group29.spacemerchant.util;
+
+import java.util.List;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
 import edu.gatech.cs2340.group29.spacemerchant.model.*;
 
 public class PlayerDataSource
 {
+    private static String[] ALL_COLUMNS = { "name", "money",
+            "pilotSkillPoints", "fighterSkillPoints", "traderSkillPoints",
+            "engineerSkillPoints" };
+    
     private SQLiteDatabase database;
     private DatabaseHelper databaseHelper;
-    
-    public PlayerDataSource( Context context )
+
+    public PlayerDataSource(Context context)
     {
-        databaseHelper = new DatabaseHelper( context );
+        databaseHelper = new DatabaseHelper(context);
     }
-    
+
     public void open() throws SQLiteException
     {
         database = databaseHelper.getWritableDatabase();
     }
-    
+
     public void close()
     {
         databaseHelper.close();
     }
-    
-    public void createPlayer(Player player) 
+
+    public void createPlayer(Player player)
     {
         ContentValues values = new ContentValues();
-       
+
         String name = player.getName();
         int money = player.getMoney();
         int[] stats = player.getStats();
-        
+
         values.put("name", name);
         values.put("money", money);
         values.put("pilotSkillPoints", stats[0]);
         values.put("fighterSkillPoints", stats[1]);
         values.put("traderSkillPoints", stats[2]);
         values.put("engineerSkillPoints", stats[3]);
-        
+
         long playerID = database.insert("tb_player", null, values);
 
         player.setID(playerID);
         return;
     }
-    
+
     public void deletePlayer(Player player)
     {
         long playerID = player.getID();
-        
+
         database.delete("tb_player", "player=" + playerID, null);
     }
+
+    public List<Player> getPlayerList()
+    {
+        return null;
+    }
+
+    public Player getPlayerByID(long playerID)
+    {
+
+        Cursor cursor = database.query("tb_player", ALL_COLUMNS, null, null, null,
+                null, null);
+      
+        cursor.moveToFirst();
+        
+        Player player = cursorToPlayer(cursor);
+        
+        return null;
+    }
+
+    public Player cursorToPlayer(Cursor cursor)
+    {
+        
+        Player player = new Player();
+       
+        int[] stats = {cursor.getInt(3),
+                       cursor.getInt(4),
+                       cursor.getInt(5),
+                       cursor.getInt(6)
+        };
+        
     
+        player.setID(cursor.getLong(0));
+        player.setName(cursor.getString(1));
+        player.setMoney(cursor.getInt(2));
+        player.setStats(stats);
+       
+        return player;
+    }
 }
