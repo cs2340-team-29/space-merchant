a24aa1e0a321721eeeece324d1e1925d1c22a2a7
rmowlavi <rmowlavi@gmail.com>
2012-10-10 21:57:15 -0400

    Misc

--- Android/SpaceMerchant/src/edu/gatech/cs2340/group29/spacemerchant/util/PlayerDataSource.java
+++ Android/SpaceMerchant/src/edu/gatech/cs2340/group29/spacemerchant/util/PlayerDataSource.java
@@ -1,5 +1,6 @@
 package edu.gatech.cs2340.group29.spacemerchant.util;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import android.content.ContentValues;
@@ -14,7 +15,7 @@ public class PlayerDataSource
     private static String[] ALL_COLUMNS = { "name", "money",
             "pilotSkillPoints", "fighterSkillPoints", "traderSkillPoints",
             "engineerSkillPoints" };
-    
+
     private SQLiteDatabase database;
     private DatabaseHelper databaseHelper;
 
@@ -63,46 +64,60 @@ public class PlayerDataSource
 
     public List<Player> getPlayerList()
     {
-        return null;
+        List<Player> players = new ArrayList<Player>();
+
+        Cursor cursor = database.query("tb_player", ALL_COLUMNS, null, null,
+                null, null, null);
+
+        cursor.moveToFirst();
+        
+        while( !cursor.isAfterLast() )
+        {
+            Player player = cursorToPlayer(cursor);
+            
+            players.add(player);
+            
+            cursor.moveToNext();
+            
+        }
+        
+        return players;
     }
 
     public Player getPlayerByID(long playerID)
     {
 
-        Cursor cursor = database.query("tb_player", ALL_COLUMNS, null, null, null,
-                null, null);
-      
+        // filter on ID condition
+        Cursor cursor = database.query("tb_player", ALL_COLUMNS, 
+                "player=" + playerID, null, null, null, null);
+
         cursor.moveToFirst();
-        
+
         Player player = cursorToPlayer(cursor);
-        
-        return null;
+
+        return player;
     }
 
     public Player cursorToPlayer(Cursor cursor)
     {
-        
+
         Player player = new Player();
-       
-        int[] stats = {cursor.getInt(3),
-                       cursor.getInt(4),
-                       cursor.getInt(5),
-                       cursor.getInt(6)
-        };
-        
-    
+
+        int[] stats = { cursor.getInt(3), cursor.getInt(4), cursor.getInt(5),
+                cursor.getInt(6) };
+
         player.setID(cursor.getLong(0));
         player.setName(cursor.getString(1));
         player.setMoney(cursor.getInt(2));
         player.setStats(stats);
-     
-        long shipID = cursor.getLong(7);
-      
-        //change this to pull actual ship based on ID
+
+        //long shipID = cursor.getLong(7);
+
+        // change this to pull actual ship based on ID
         Ship ship = new Ship();
-        
+
         player.setShip(ship);
-        
+
         return player;
     }
 }
