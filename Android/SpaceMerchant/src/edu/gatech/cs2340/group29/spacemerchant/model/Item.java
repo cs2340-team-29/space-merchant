e441094f8d3855743328520ba90f995460feb41a
Kevin Shu <kshu7@gatech.edu>
2012-10-22 19:02:56 -0400

    updated items and inventory

--- Android/SpaceMerchant/src/edu/gatech/cs2340/group29/spacemerchant/model/Item.java
+++ Android/SpaceMerchant/src/edu/gatech/cs2340/group29/spacemerchant/model/Item.java
@@ -3,33 +3,86 @@ package edu.gatech.cs2340.group29.spacemerchant.model;
 
 public class Item implements Comparable<Item>
 {
-    private int    type; //Drawable
+    private int    type;
     private int    basePrice;
     private String name;
+    private int drawable;
     
-    public Item( int type, int basePrice, String name )
+    /**
+     * creates an item
+     * 
+     * @param type
+     * @param basePrice
+     * @param name
+     * @param drawable
+     */
+    public Item( int type, int basePrice, String name , int drawable)
     {
         this.type = type;
         this.basePrice = basePrice;
         this.name = name;
+        this.drawable = drawable;
     }
     
+    /**
+     * 
+     * creates an item without a drawable
+     * 
+     * @param type
+     * @param basePrice
+     * @param name
+     */
+    public Item( int type, int basePrice, String name) {
+    	this.type = type;
+        this.basePrice = basePrice;
+        this.name = name;
+    }
+    
+    /**
+     * gets the type of item it is
+     * 
+     * @return the type
+     */
     public int getType()
     {
-        return type; //Drawable
+        return type;
     }
     
+    /**
+     * 
+     * gets the base price of the item
+     * 
+     * @return the base price
+     */
     public int getBasePrice()
     {
         return basePrice;
     }
     
+    /**
+     * 
+     * get the name of the item
+     * 
+     * @return the name
+     */
     public String getName()
     {
         return name;
     }
     
-    public int compareTo( Item itm )
+    /**
+     * 
+     * gets the resource id for drawable
+     * 
+     * @return the drawable id
+     */
+    public int getDrawable() {
+		return drawable;
+	}
+
+    
+    // compares the item to another item
+	public int compareTo( Item itm )
     {
         if ( itm.equals( this ) )
             return 0;
