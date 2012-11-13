1ab3c80727ce5580739591dee5115857378d0c35
Kemble Hildreth <kemble.hildreth@gmail.com>
2012-11-13 16:38:01 -0500

    Adding my jUnit tests

---
+++ Android/SpaceMerchant/src/edu/gatech/cs2340/group29/spacemerchant/model/KembleJUnit.java
@@ -0,0 +1,20 @@
+package edu.gatech.cs2340.group29.spacemerchant.model;
+
+import junit.framework.TestCase;
+
+public class KembleJUnit extends TestCase {
+
+	public void KembleJunit()
+	{
+	}
+	
+	public void testTravel()
+	{
+		assertTrue("Add result should be 98", ( 2==2 ));  
+	}
+	
+	public void testShipBuyer()
+	{
+		assertTrue("Add result should be 98", ( 2==3 ));  
+	}
+}
