fa0484a7d7e76f34e45d25851fa40c5114075697
Kemble Hildreth <kemble.hildreth@gmail.com>
2012-11-13 18:42:23 -0500

    Added final j-unit test

--- Android/SpaceMerchant/src/edu/gatech/cs2340/group29/spacemerchant/model/KembleJUnit.java
+++ Android/SpaceMerchant/src/edu/gatech/cs2340/group29/spacemerchant/model/KembleJUnit.java
@@ -1,28 +1,16 @@
 package edu.gatech.cs2340.group29.spacemerchant.model;
 
+import android.content.Context;
 import junit.framework.TestCase;
+import edu.gatech.cs2340.group29.spacemerchant.interfaces.Marketable;
 
 public class KembleJUnit extends TestCase {
-
 	
 	/**
-	 * These J-Unit tests will asses the functionality of the way items work,
-	 * as well as some inventory functions, and Market functions
+	 * This JUnit test asseses if the getBasePrice for item is properly working
 	 */
 	public void KembleJunit()
 	{
-	}
-		
-	public void testItem()
-	{
-		// intakes type, name, drawable, techLevel
-		Item itm1 = new Item( 1, "widget", 1, 3 );
-		Item itm2 = new Item( 0, "monkey", 2, 1 );
-		Item itm3 = new Item( 2, "sedan", 3, 2 );
-		Item itm4 = new Item( 4, "Nixon", 5, 1 );
-		Item itm5 = new Item( 3, "Chips", 6, 2 );
-		
-		assertTrue("woohoo truee", ( 2==2 ));  
 	}
 	
 	public void testItem_getBasePrice()
@@ -35,6 +23,5 @@ public class KembleJUnit extends TestCase {
 		System.out.println( "Game method base price: "+calculated_base_price );
 		
 		assertTrue("Base price method works properly!", ( itm1.getBasePrice() == calculated_base_price ));
-	}
-	
+	}	
 }
