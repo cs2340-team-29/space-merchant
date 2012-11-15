303f05d0b7b6d88c299efdf835e8c46d65d06bda
Kevin Shu <kshu7@gatech.edu>
2012-11-14 19:02:37 -0500

    Added my JUnit Test

---
+++ Android/SpaceMerchant/src/edu/gatech/cs2340/group29/spacemerchant/model/KevinJUnit.java
@@ -0,0 +1,43 @@
+package edu.gatech.cs2340.group29.spacemerchant.model;
+
+import junit.framework.TestCase;
+
+public class KevinJUnit extends TestCase {
+
+	public void testRemove() {
+		int capacity = 10;
+		Inventory inventory = new Inventory(capacity);
+		Item item = new Item(0, "Item1", 0, 0);
+		Item ret;
+		try {
+			ret = inventory.remove(item);
+			assertTrue("Successfully checked for removing from an empty inventory",ret == null);
+		}
+		catch(Exception e) {
+			fail("Did not check for removing from an empty inventory");
+		}
+		inventory.add(item);
+		try {
+			ret = inventory.remove(item);
+			assertTrue("Successful in removing item from one item inventory", ret == item);
+		}
+		catch(Exception e) {
+			fail("Error when removing item from one item inventory");
+		}
+		
+		for (int i = 0; i < capacity - 1; i++) {
+			inventory.add(item);
+		}
+		Item item2 = new Item(1, "Item2", 1, 1);
+		inventory.add(item2);
+		try {
+			ret = inventory.remove(item2);
+			assertTrue("Successful in removing item from full inventory", ret == item2);
+		}
+		catch(Exception e) {
+			fail("Error when removing item from full inventory");
+		}
+		
+	}
+
+}
