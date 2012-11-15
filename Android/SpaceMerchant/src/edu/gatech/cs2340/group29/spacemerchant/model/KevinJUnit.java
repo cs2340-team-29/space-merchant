12c84162c5adef65f188e00171ab3a6af12a4099
Kevin Shu <kshu7@gatech.edu>
2012-11-14 22:28:16 -0500

    Edited my JUnit

--- Android/SpaceMerchant/src/edu/gatech/cs2340/group29/spacemerchant/model/KevinJUnit.java
+++ Android/SpaceMerchant/src/edu/gatech/cs2340/group29/spacemerchant/model/KevinJUnit.java
@@ -7,11 +7,12 @@ public class KevinJUnit extends TestCase {
 	public void testRemove() {
 		int capacity = 10;
 		Inventory inventory = new Inventory(capacity);
+		Inventory inventory2 = inventory;
 		Item item = new Item(0, "Item1", 0, 0);
 		Item ret;
 		try {
 			ret = inventory.remove(item);
-			assertTrue("Successfully checked for removing from an empty inventory",ret == null);
+			assertTrue("Check removing from an empty inventory",ret == null);
 		}
 		catch(Exception e) {
 			fail("Did not check for removing from an empty inventory");
@@ -19,20 +20,21 @@ public class KevinJUnit extends TestCase {
 		inventory.add(item);
 		try {
 			ret = inventory.remove(item);
-			assertTrue("Successful in removing item from one item inventory", ret == item);
+			assertTrue("Check removing item from one-item inventory", ret == item && inventory.equals(inventory2));
 		}
 		catch(Exception e) {
-			fail("Error when removing item from one item inventory");
+			fail("Error when removing item from one-item inventory");
 		}
 		
 		for (int i = 0; i < capacity - 1; i++) {
 			inventory.add(item);
 		}
+		inventory2 = inventory;
 		Item item2 = new Item(1, "Item2", 1, 1);
 		inventory.add(item2);
 		try {
 			ret = inventory.remove(item2);
-			assertTrue("Successful in removing item from full inventory", ret == item2);
+			assertTrue("Check removing item from full inventory", ret == item2 && inventory.equals(inventory2));
 		}
 		catch(Exception e) {
 			fail("Error when removing item from full inventory");
